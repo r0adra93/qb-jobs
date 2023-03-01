718f8aecb0b78d848d69349d81be67e546724576
zwkf <dev@charlie.fyi>
2023-03-01 19:29:25 +0000

    Update garbage.lua

--- jobs/garbage.lua
+++ jobs/garbage.lua
@@ -2,7 +2,7 @@ Config.Jobs = Config.Jobs or {}
 Config.Jobs.garbage = {
     ["label"] = "Sanitation Engineer",
     ["webHooks"] = {
-        ["garbage"] = "https://discord.com/api/webhooks/1068882053430517823/1_GeFJUZPbdI15MPoX0Iq8PgUE8BINl3OO74vV3J6c2tJXUJsHMTeDrVu1mbY_dZWD3P"
+        ["garbage"] = "https://discord.com/api/webhooks/..."
     },
     ["defaultDuty"] = true,
     ["offDutyPay"] = false,
