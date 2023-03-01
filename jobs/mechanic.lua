124fd7afaa6246ae3d1cc37e72a915c818b7116f
zwkf <dev@charlie.fyi>
2023-03-01 19:30:09 +0000

    Update mechanic.lua

--- jobs/mechanic.lua
+++ jobs/mechanic.lua
@@ -2,7 +2,7 @@ Config.Jobs = Config.Jobs or {}
 Config.Jobs.mechanic = {
     ["label"] = "mechanic",
     ["webHooks"] = {
-        ["mechanic"] = "https://discord.com/api/webhooks/1068882053430517823/1_GeFJUZPbdI15MPoX0Iq8PgUE8BINl3OO74vV3J6c2tJXUJsHMTeDrVu1mbY_dZWD3P"
+        ["mechanic"] = "https://discord.com/api/webhooks/..."
     },
     ["defaultDuty"] = true,
     ["offDutyPay"] = false,
