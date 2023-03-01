1edb2d9054ff0221062bfb88c3cd0de218cfe073
zwkf <dev@charlie.fyi>
2023-03-01 19:29:41 +0000

    Update judge.lua

--- jobs/judge.lua
+++ jobs/judge.lua
@@ -3,7 +3,7 @@ Config.Jobs = Config.Jobs or {}
 Config.Jobs.judge = {
     ["label"] = "judge",
     ["webHooks"] = {
-        ["judge"] = "https://discord.com/api/webhooks/1068882053430517823/1_GeFJUZPbdI15MPoX0Iq8PgUE8BINl3OO74vV3J6c2tJXUJsHMTeDrVu1mbY_dZWD3P"
+        ["judge"] = "https://discord.com/api/webhooks/..."
     },
     ["defaultDuty"] = true,
     ["offDutyPay"] = true,
