83ed4c18deda6bc8f8c9300b0ec3720ceb335c59
zwkf <dev@charlie.fyi>
2023-03-01 19:29:16 +0000

    Update cardealer.lua

--- jobs/cardealer.lua
+++ jobs/cardealer.lua
@@ -2,7 +2,7 @@ Config.Jobs = Config.Jobs or {}
 Config.Jobs.cardealer = {
     ["label"] = "Vehicle Dealer",
     ["webHooks"] = {
-        ["cardealer"] = "https://discord.com/api/webhooks/1068882053430517823/1_GeFJUZPbdI15MPoX0Iq8PgUE8BINl3OO74vV3J6c2tJXUJsHMTeDrVu1mbY_dZWD3P"
+        ["cardealer"] = "https://discord.com/api/webhooks/..."
     },
     ["defaultDuty"] = true,
     ["offDutyPay"] = false,
